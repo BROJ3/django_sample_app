ffdbfbe602af3caf27505b5dbb53df1bb000ad12
BROJ3 <toni.crnjak@hotmail.com>
2023-12-27 07:43:20 -0500

    create database model

--- django_techwithtim/demo/myapp/models.py
+++ django_techwithtim/demo/myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+#creating database model 
+class TodoItem(models.Model):
+    title = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
