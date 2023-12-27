0a0d7605988ca542393ee0d0ca4246b03fdbd158
BROJ3 <toni.crnjak@hotmail.com>
2023-12-27 07:53:42 -0500

    connect todos and update urls

--- django_techwithtim/demo/myapp/views.py
+++ django_techwithtim/demo/myapp/views.py
@@ -5,6 +5,8 @@ from .models import TodoItem
 def home(request):
     return render(request, "home.html")
 
+
+
 def todos(request):
     items = TodoItem.objects.all()
     return render(request,"todos.html",{"todos":items})
